8f09e53c562d2d16c4d897d2414d5f10f33e869f
Jeamn <jeamnchan@gmail.com>
2023-10-19 23:28:12 +0800

    fix: auto order

--- .dumirc.ts
+++ .dumirc.ts
@@ -42,133 +42,89 @@ export default defineConfig({
       '/line': [
         {
           title: '📈 折线图',
-          children: [
-            { title: '基础折线图', link: '/line/basic-line-chart', order: 1 },
-            {
-              title: '基础平滑折线图',
-              link: '/line/smoothed-line-chart',
-              order: 2,
-            },
-            { title: '基础面积图', link: '/line/basic-area-chart', order: 3 },
-            { title: '折线图堆叠', link: '/line/stacked-line-chart', order: 4 },
-            { title: '堆叠面积图', link: '/line/stacked-area-chart', order: 5 },
+          children: addOrder([
+            { title: '基础折线图', link: '/line/basic-line-chart' },
+            { title: '基础平滑折线图', link: '/line/smoothed-line-chart' },
+            { title: '基础面积图', link: '/line/basic-area-chart' },
+            { title: '折线图堆叠', link: '/line/stacked-line-chart' },
+            { title: '堆叠面积图', link: '/line/stacked-area-chart' },
             {
               title: '渐变堆叠面积图',
               link: '/line/gradient-stacked-area-chart',
-              order: 6,
             },
-            { title: '凹凸图', link: '/line/bump-chart-ranking', order: 7 },
+            { title: '凹凸图', link: '/line/bump-chart-ranking' },
             {
               title: '未来一周气温变化',
               link: '/line/temperature-change-in-the-coming-week',
-              order: 8,
             },
-            { title: '折线图区域高亮', link: '/line/area-pieces', order: 9 },
-            {
-              title: '数据过滤',
-              link: '/line/data-transform-filter',
-              order: 10,
-            },
-            { title: '线图的渐变', link: '/line/line-gradient', order: 11 },
+            { title: '折线图区域高亮', link: '/line/area-pieces' },
+            { title: '数据过滤', link: '/line/data-transform-filter' },
+            { title: '线图的渐变', link: '/line/line-gradient' },
             {
               title: '一天用电量分布',
               link: '/line/distribution-of-electricity',
-              order: 12,
             },
-            {
-              title: '大数据量面积图',
-              link: '/line/large-scale-area-chart',
-              order: 13,
-            },
-            {
-              title: 'Confidence Band',
-              link: '/line/confidence-band',
-              order: 14,
-            },
+            { title: '大数据量面积图', link: '/line/large-scale-area-chart' },
+            { title: 'Confidence Band', link: '/line/confidence-band' },
             {
               title: '雨量Evaporation关系图',
               link: '/line/rainfall-vs-evaporation',
-              order: 15,
             },
-            { title: '北京 AQI 可视化', link: '/line/beijing-aqi', order: 16 },
-            { title: '多 X 轴', link: '/line/multiple-x-axes', order: 17 },
-            { title: '雨量流量关系图', link: '/line/rainfall', order: 18 },
-            {
-              title: '时间轴折线图',
-              link: '/line/area-chart-with-time-axis',
-              order: 19,
-            },
+            { title: '北京 AQI 可视化', link: '/line/beijing-aqi' },
+            { title: '多 X 轴', link: '/line/multiple-x-axes' },
+            { title: '雨量流量关系图', link: '/line/rainfall' },
+            { title: '时间轴折线图', link: '/line/area-chart-with-time-axis' },
             {
               title: '动态数据 + 时间坐标轴',
               link: '/line/dynamic-data-time-axis',
-              order: 20,
             },
-            {
-              title: '函数绘图',
-              link: '/line/function-plot',
-              order: 21,
-            },
-            {
-              title: '动态排序折线图',
-              link: '/line/line-race',
-              order: 22,
-            },
-            {
-              title: '折线图的标记线',
-              link: '/line/line-with-marklines',
-              order: 23,
-            },
+            { title: '函数绘图', link: '/line/function-plot' },
+            { title: '动态排序折线图', link: '/line/line-race' },
+            { title: '折线图的标记线', link: '/line/line-with-marklines' },
             {
               title: '自定义折线图样式',
               link: '/line/line-style-and-item-style',
-              order: 24,
             },
-          ],
+          ]),
         },
       ],
       '/bar': [
         {
           title: '📊 柱状图',
-          children: [
-            {
-              title: '带背景色的柱状图',
-              link: '/bar/bar-with-background',
-              order: 1,
-            },
-            { title: '基础柱状图', link: '/bar/basic-bar', order: 2 },
+          children: addOrder([
+            { title: '带背景色的柱状图', link: '/bar/bar-with-background' },
+            { title: '基础柱状图', link: '/bar/basic-bar' },
             {
               title: '坐标轴刻度与标签对齐',
               link: '/bar/axis-align-with-tick',
-              order: 3,
             },
             {
               title: '自定义单个柱子颜色',
               link: '/bar/set-style-of-single-bar',
-              order: 4,
             },
-            {
-              title: '瀑布图（柱状图模拟）',
-              link: '/bar/waterfall-chart',
-              order: 5,
-            },
+            { title: '瀑布图（柱状图模拟）', link: '/bar/waterfall-chart' },
             {
               title: '交错正负轴标签',
               link: '/bar/bar-chart-with-negative-value',
-              order: 6,
             },
             {
               title: '极坐标柱状图标签',
               link: '/bar/radial-polar-bar-label-position',
-              order: 7,
             },
             {
               title: '极坐标柱状图标签',
               link: '/bar/tangential-polar-bar-label-position',
-              order: 8,
             },
-          ],
+          ]),
         },
       ],
     },
   },
 });
+
+function addOrder(list: { title: string; link: string }[]) {
+  return list.map((i, index) => ({
+    ...i,
+    order: index + 1,
+  }));
+}
